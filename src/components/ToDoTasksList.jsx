05095113dbcfcda0d1006dc072e57812caf7b1a5
Muhammad-Taha-Qader <m.tahaqader000@gmail.com>
2024-10-10 20:11:33 +0500

    UI updates

--- src/components/ToDoTasksList.jsx
+++ src/components/ToDoTasksList.jsx
@@ -10,7 +10,7 @@ function ToDoTasksList({ list, setList }) {
       {list?.map((item, index) => (
         <div key={index} className="flex px-6 py-2 gap-x-5 items-center">
           <input type="checkbox" name="checked" id="checked" />
-          <span className="flex-grow">{item}</span>
+          <span className="flex-grow break-all text-wrap">{item}</span>
           <button 
             type="button" 
             onClick={() => handleDelete(index)}
